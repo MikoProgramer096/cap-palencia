070d8655597bb150bd531c9cf4db1843b770931d
Andrxs <ricoandres096@gmail.com>
2026-04-09 05:40:42 -0500

    agregar paginas inicio categorias ofertas contacto

--- N-MAD/src/main/java/com/andres/N/MAD/controller/CatalogoController.java
+++ N-MAD/src/main/java/com/andres/N/MAD/controller/CatalogoController.java
@@ -1,7 +1,6 @@
 package com.andres.N.MAD.controller;
 
 import com.andres.N.MAD.model.Gorra;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,17 +12,32 @@ import java.util.List;
 public class CatalogoController {
 
     @GetMapping("/")
-    public String mostrarCatalogo(Model model) {
-
-        List<Gorra> gorras = new ArrayList<>();
-
-        gorras.add(new Gorra(" Gorra Blanca Basica", "New era", 65.51, "/images/Gorra1.jpg"));
-        gorras.add(new Gorra("Gorra Azul Clasica", "New era", 65.51, "/images/Gorra2.jpg"));
-        gorras.add(new Gorra("chicago white sox", "New era", 65.51, "/images/Gorra3.png"));
-
-        model.addAttribute("gorras", gorras);
-
-        return "index";
+    public String inicio(Model model) {
+        return "inicio";
     }
 
+    @GetMapping("/catalogo")
+    public String catalogo(Model model) {
+        List<Gorra> gorras = new ArrayList<>();
+    gorras.add(new Gorra("Gorra Blanca Basica", "New Era", 65.51, "/images/Gorra1.jpg"));
+    gorras.add(new Gorra("Gorra Azul Clasica", "New Era", 65.51, "/images/Gorra2.jpg"));
+    gorras.add(new Gorra("Chicago White Sox", "New Era", 65.51, "/images/Gorra3.png"));
+        model.addAttribute("gorras", gorras);
+        return "catalogo";
+    }
+
+    @GetMapping("/categorias")
+    public String categorias(Model model) {
+        return "categorias";
+    }
+
+    @GetMapping("/ofertas")
+    public String ofertas(Model model) {
+        return "ofertas";
+    }
+
+    @GetMapping("/contacto")
+    public String contacto(Model model) {
+        return "contacto";
+    }
 }
